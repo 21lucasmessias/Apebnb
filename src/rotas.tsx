d20958d2ecddb6ac2283607c76d0432a64eef795
21lucasmessias <21.lucasmessias@gmail.com>
2021-07-04 13:05:41 -0300

    #34 - feature: Configuration on routes to use MaterialBottomTabNavigation

--- src/rotas.tsx
+++ src/rotas.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 
 import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Inicio } from "./telas/Inicio";
+
+const { Navigator, Screen } = createMaterialBottomTabNavigator();
 
 export const Rotas = () => {
   return (
     <NavigationContainer>
+      <Navigator>
+        <Screen component={Inicio} name='inicio'/>
+      </Navigator>
     </NavigationContainer>
   );
 }
