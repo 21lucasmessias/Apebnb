81425fe203352f7580078f81c9a315e000775dae
21lucasmessias <21.lucasmessias@gmail.com>
2021-07-13 23:53:19 -0300

    #12 - feature: Added ResidentCard

---
+++ src/componentes/CartaoMorador/index.tsx
@@ -0,0 +1,40 @@
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
+import React from 'react'
+
+import Icon from 'react-native-vector-icons/Feather'
+import { tema } from '../../global/estilos/tema'
+import { iMorador } from '../../models/Morador'
+import { RotasParamsList } from '../../rotas'
+
+import {
+  Envolvedor,
+  Foto,
+  Texto,
+  FotoVaziaEnvolvedor,
+  Pressionavel
+} from './estilos'
+
+interface iCartaoMorador {
+  morador: iMorador,
+  navigation: BottomTabNavigationProp<RotasParamsList>
+}
+
+const CartaoMorador: React.FC<iCartaoMorador> = ({ morador, navigation }) => {
+  return (
+    <Pressionavel activeOpacity={0.7}>
+      <Envolvedor>
+        {morador.foto ? (
+          <Foto source={{uri: morador.foto}}/>
+        ) : (
+          <FotoVaziaEnvolvedor>
+            <Icon name='camera' size={24} color={tema.color.azulEscuro}/>
+          </FotoVaziaEnvolvedor>
+        )}
+        
+        <Texto>{morador.nome}</Texto>
+      </Envolvedor>
+    </Pressionavel>
+  )
+}
+
+export default CartaoMorador
